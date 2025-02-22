7d98af398f2081664cfa6b86985197fa185b1de5
JasnoorMaan <jasnoorm0001@gmail.com>
2025-02-22 18:35:45 +0530

    supabase init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect , useState} from "react";
 import Loading from "./components/Loading";
 const Hero = lazy(() => import("./components/Hero"));
 const Uses = lazy(() => import("./components/Uses"));
 const Navbar = lazy(() => import("./components/Navbar"));
 const Footer = lazy(() => import("./components/Footer"));
+import {supabase} from "./supabaseClient";
 
 function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({data:{session}})=>{
+      setSession(session);
+    })
+    const {data}= supabase.auth.onAuthStateChange((_event,session)=>{
+      setSession(session);
+    })
+
+    return()=>{
+      data.subscription.unsubscribe();
+    }
+  },[]);
+
   return (
     <>
       <Suspense fallback={<Loading />}>
-        <Navbar />
+        <Navbar key={session?.user?.id || 'no-session'} session={session}/>
         <Hero />
         <Uses />
         <Footer />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,34 @@
 import "../App.css";
 import { Link } from "react-router";
-const Navbar = () => {
+import {supabase} from "../supabaseClient";
+
+const Navbar = ({session}) => {
+
+  const handleLogin= async ()=>{
+    const {error} = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+    if(error){
+      console.error("Error signing in: ",error.message);
+    }
+  };
+
   return (
     <>
       <div className="navbar">
         <Link to="/" className="logo">
           Genie.
         </Link>
+        <div>
+          {!session ? (<button className="nav-button" onClick={handleLogin}>
+          <h2 className="bodyText">Log In/Sign Up</h2>
+          </button>): (
+            <button className="nav-button" onClick={()=>supabase.auth.signOut()}>
+              <h2 className="bodyText">{session.user.email}</h2>
+            </button>
+          ) 
+          }
+        </div>
       </div>
     </>
   );
